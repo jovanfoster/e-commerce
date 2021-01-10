e9867571ac8c4298648853261b928250f40507dd
Jovan <jovanfoster9@gmail.com>
2021-01-10 13:44:42 -0500

    added host to allowed hosts

--- ecommerce_website/settings.py
+++ ecommerce_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['lit-peak-45044.herokuapp.com/', 'localhost', '127.0.0.1']
